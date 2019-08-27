d1708d6410b4d19d0ebe75d7e11179f3079ad442
Dan Harrin <git@DanHarrin.com>
2019-08-27 21:12:36 +0100

    Disable kick and ban

--- index.js
+++ index.js
@@ -42,79 +42,79 @@ client.on('message', async (message) => {
 	// 	message.channel.send(sayMessage);
 	// }
 
-	if (command === 'kick') {
-		if (
-			!message.member.roles.some((r) =>
-				['Administrator', 'Moderator'].includes(r.name),
-			)
-		)
-			return message.reply(
-				"Sorry, you don't have permissions to use this!",
-			);
+	// if (command === 'kick') {
+	// 	if (
+	// 		!message.member.roles.some((r) =>
+	// 			['Administrator', 'Moderator'].includes(r.name),
+	// 		)
+	// 	)
+	// 		return message.reply(
+	// 			"Sorry, you don't have permissions to use this!",
+	// 		);
 
-		let member =
-			message.mentions.members.first() ||
-			message.server.members.get(args[0]);
-		if (!member)
-			return message.reply(
-				'Please mention a valid member of this server',
-			);
-		if (!member.kickable)
-			return message.reply(
-				'I cannot kick this user! Do they have a higher role? Do I have kick permissions?',
-			);
+	// 	let member =
+	// 		message.mentions.members.first() ||
+	// 		message.server.members.get(args[0]);
+	// 	if (!member)
+	// 		return message.reply(
+	// 			'Please mention a valid member of this server',
+	// 		);
+	// 	if (!member.kickable)
+	// 		return message.reply(
+	// 			'I cannot kick this user! Do they have a higher role? Do I have kick permissions?',
+	// 		);
 
-		let reason = args.slice(1).join(' ');
-		if (!reason) reason = 'No reason provided';
+	// 	let reason = args.slice(1).join(' ');
+	// 	if (!reason) reason = 'No reason provided';
 
-		await member
-			.kick(reason)
-			.catch((error) =>
-				message.reply(
-					`Sorry ${message.author} I couldn't kick because of : ${error}`,
-				),
-			);
-		message.reply(
-			`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`,
-		);
-	}
+	// 	await member
+	// 		.kick(reason)
+	// 		.catch((error) =>
+	// 			message.reply(
+	// 				`Sorry ${message.author} I couldn't kick because of : ${error}`,
+	// 			),
+	// 		);
+	// 	message.reply(
+	// 		`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`,
+	// 	);
+	// }
 
-	if (command === 'ban') {
-		if (
-			!message.member.roles.some((r) =>
-				['Administrator'].includes(r.name),
-			)
-		)
-			return message.reply(
-				"Sorry, you don't have permissions to use this!",
-			);
+	// if (command === 'ban') {
+	// 	if (
+	// 		!message.member.roles.some((r) =>
+	// 			['Administrator'].includes(r.name),
+	// 		)
+	// 	)
+	// 		return message.reply(
+	// 			"Sorry, you don't have permissions to use this!",
+	// 		);
 
-		let member = message.mentions.members.first();
-		if (!member)
-			return message.reply(
-				'Please mention a valid member of this server',
-			);
-		if (!member.bannable)
-			return message.reply(
-				'I cannot ban this user! Do they have a higher role? Do I have ban permissions?',
-			);
+	// 	let member = message.mentions.members.first();
+	// 	if (!member)
+	// 		return message.reply(
+	// 			'Please mention a valid member of this server',
+	// 		);
+	// 	if (!member.bannable)
+	// 		return message.reply(
+	// 			'I cannot ban this user! Do they have a higher role? Do I have ban permissions?',
+	// 		);
 
-		let reason = args.slice(1).join(' ');
-		if (!reason) reason = 'No reason provided';
+	// 	let reason = args.slice(1).join(' ');
+	// 	if (!reason) reason = 'No reason provided';
 
-		await member
-			.ban(reason)
-			.catch((error) =>
-				message.reply(
-					`Sorry ${message.author} I couldn\'t ban because of : ${error}`,
-				),
-			);
-		message.reply(
-			`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`,
-		);
-	}
+	// 	await member
+	// 		.ban(reason)
+	// 		.catch((error) =>
+	// 			message.reply(
+	// 				`Sorry ${message.author} I couldn\'t ban because of : ${error}`,
+	// 			),
+	// 		);
+	// 	message.reply(
+	// 		`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`,
+	// 	);
+	// }
 
-	if (command === 'purge') {
+	if (command === 'clear') {
 		const deleteCount = parseInt(args[0], 10);
 
 		if (!deleteCount || deleteCount < 2 || deleteCount > 100)
