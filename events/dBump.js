5ce3c62a0d5f2a1d6465a00e2ca3aa69f159b780
Dan Harrin <git@DanHarrin.com>
2019-08-28 11:12:29 +0100

    Nice try! bump

--- events/dBump.js
+++ events/dBump.js
@@ -1,7 +1,16 @@
 module.exports = (msg) => {
 	if (msg.content.includes('Bump done') && msg.author.bot) {
-			return msg.channel.send(
-				`Thanks for the bump, ${msg.mentions.members.first()}!`,
-			);
-		}
+		return msg.channel.send(
+			`Thanks for the bump, ${msg.mentions.members.first()}!`,
+		);
+	}
+	
+	if (
+		msg.content.includes('until the server can be bumped') &&
+		msg.author.bot
+	) {
+		return msg.channel.send(
+			`Nice try, ${msg.mentions.members.first()}!`,
+		);
+	}
 }
