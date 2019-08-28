dfaa906ee59004fa1614acaaa4b3a1ffc7f14049
Dan Harrin <git@DanHarrin.com>
2019-08-28 14:38:50 +0100

    Fix

--- events/dBump.js
+++ events/dBump.js
@@ -1,13 +1,13 @@
 module.exports = (msg) => {
-	if (msg.embeds[0].description.includes('Bump done')) {
-		return msg.channel.send(
-			`Thanks for the bump, ${msg.mentions.members.first()}!`,
-		);
-	}
+	// if (msg.embeds.description.includes('Bump done')) {
+	// 	return msg.channel.send(
+	// 		`Thanks for the bump, ${msg.mentions.members.first()}!`,
+	// 	);
+	// }
 
-	if (
-		msg.embeds[0].description.includes('until the server can be bumped')
-	) {
-		return msg.channel.send(`Nice try, ${msg.mentions.members.first()}!`);
-	}
+	// if (
+	// 	msg.embeds.description.includes('until the server can be bumped')
+	// ) {
+	// 	return msg.channel.send(`Nice try, ${msg.mentions.members.first()}!`);
+	// }
 }
