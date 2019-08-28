7c945207e3fdb3094a139f350d210781409512e1
Dan Harrin <git@DanHarrin.com>
2019-08-28 10:45:15 +0100

    Remove logging

--- utils/checkRoles.js
+++ utils/checkRoles.js
@@ -1,4 +1,3 @@
 module.exports = async (member, roles) => {
-	console.log(member.roles.some((role) => roles.includes(role.name)));
 	return member.roles.some((role) => roles.includes(role.name));
 };
