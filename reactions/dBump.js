e9756283c9b3b401e05fb74f526252896a3f79b7
Dan Harrin <git@DanHarrin.com>
2019-08-28 09:36:25 +0100

    Remove bot check

--- reactions/dBump.js
+++ reactions/dBump.js
@@ -1,5 +1,5 @@
 module.exports = (message) => {
-    if (message.content.includes('Bump done') && message.author.bot) {
+    if (message.content.includes('Bump done')) {
 		return message.channel.send(
 			`Thanks for the bump, ${message.mentions.members.first()}!`,
 		);
